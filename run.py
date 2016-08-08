7d6c5e799e41dfbba6b95550e4ee7b8078a6a9a6
Joongi Kim <me@daybreaker.info>
2016-08-08 16:16:25 +0900

    Always use UTF-8 encoding.

--- run.py
+++ run.py
@@ -16,14 +16,15 @@ if __name__ == '__main__':
     argparser.add_argument('-s', '--style', dest='style', default='bw')
     args = argparser.parse_args()
 
-    input_code = sys.stdin.read()
-    lexer = pygments.lexers.get_lexer_by_name(args.lexer)
+    input_code = sys.stdin.buffer.read()
+    lexer = pygments.lexers.get_lexer_by_name(args.lexer, encoding='utf-8')
     formatter = pygments.formatters.get_formatter_by_name('html',
+        encoding='utf-8',
         full=True,
         cssfile='weighted.css',
         noclobber_cssfile=True,
         style=args.style)
 
-    with open('output.html', 'w', encoding='utf8') as outfile:
+    with open('output.html', 'wb') as outfile:
         pygments.highlight(input_code, lexer, formatter, outfile=outfile)
 
